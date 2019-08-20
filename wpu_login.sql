1adeb79aa53bfa8c8f40e43b1a65a7f582564609
rama <arif.ramadhan@students.amikom.ac.id>
2019-08-20 18:50:03 +0700

    3

--- wpu_login.sql
+++ wpu_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 14, 2019 at 05:04 PM
+-- Generation Time: Aug 20, 2019 at 01:49 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -44,8 +44,54 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(1, 'rama', 'arif@rama.com', 'default.jpg', '$2y$10$cLP.uKWqKhu5B0gFqtnemuIYa3F1amS8Zss1AQR77cNJkRm4XmlnO', 2, 1, 1565683321),
-(2, 'joko', 'arif@rama.co.id', 'default.jpg', '$2y$10$037GyBHUqEMinYWTxDUGxO02lDvSdVPn69b8JSuLFrabmWyc1eiF6', 2, 1, 1565683776);
+(3, 'ramadhan', 'arif@rama.com', 'rama.png', '$2y$10$i4MOK8y2PHtZ/YoTDzPsXOhTxSf6LtC1UP5hSIsYorEEFXYLMjHmq', 2, 1, 1566114341),
+(4, 'admin', 'admin@gmail.com', 'index.jpg', '$2y$10$a1MWelwwAhlBHCEx70.pjOQCbf.0089YeHoEB0hvuC8eMvA.fIKH6', 1, 1, 1566116329),
+(5, 'sjhsjahsj', 'jasjj@jsajhs.sks', 'default.jpg', '$2y$10$h8m0m1o1jvqD80IWmaljXeUD7z34mTQk1rJh2K0ONIDWB4cKiz9QC', 2, 0, 1566294764),
+(8, 'rama', 'ariframdhan833@gmail.com', 'default.jpg', '$2y$10$Yu0zuCJk6E1xrncUtrq5fuwdlcOfDN1XgPE.36HFbehPnN8PY0BLK', 2, 0, 1566294979);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(5, 1, 2),
+(6, 1, 3),
+(7, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu'),
+(4, 'Buku'),
+(5, 'Role');
 
 -- --------------------------------------------------------
 
@@ -66,6 +112,55 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Member');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(3, 3, 'Menu Manajemen', 'menu', 'fas fa-fw fa-folder', 1),
+(4, 3, 'Submenu Manajemen', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(5, 1, 'cek', 'admin/cek', 'fab fa-fw fa-youtube', 1),
+(6, 1, 'Role', 'admin/role', 'fas fa-fw fa-folder-open', 1),
+(7, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-tie', 1),
+(8, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_token`
+--
+
+INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
+(1, 'ariframdhan833@gmail.com', 'c0VfFYwPtA+9QjjTz/RwaCKkuU/8JLEGcHNBrX3MYpU=', 1566294979);
+
 --
 -- Indexes for dumped tables
 --
@@ -77,9 +172,33 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -90,13 +209,37 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
